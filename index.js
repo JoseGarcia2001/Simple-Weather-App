e947521358c8abc96bbac887f933e2d6f1956e0b
Jose Garcia <jagarcia7655@gmail.com>
2021-05-10 21:13:39 -0500

    Creating styles

---
+++ index.js
@@ -0,0 +1,3 @@
+const apiKey = "21d807407fc08f0dca752a98c2c9b2d1";
+const imgURL = `http://openweathermap.org/img/wn/${code}@2x.png`;
+const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=sp`;
